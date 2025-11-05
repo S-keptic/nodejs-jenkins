4dcc5748c6fc3a4c236b916e2ac060bf0a2e75cb
Anmol <anmol.volt@gmail.com>
2025-11-05 11:56:22 +0000

    v2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/',(req,res)=>{
-    res.send("Hello from Jenkins CI/CD")
+    res.send("Hello from Jenkins CI/CD version 2")
 })
 
 app.listen(3000,()=>{
