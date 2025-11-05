ed2bf264913dac3dff25408a4a8a4863abb3fe8d
Anmol <anmol.volt@gmail.com>
2025-11-05 09:47:55 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.get('/',(req,res)=>{
+    res.send("Hello from Jenkins CI/CD")
+})
+
+app.listen(3000,()=>{
+    console.log("Server running on Port 3000")
+})
